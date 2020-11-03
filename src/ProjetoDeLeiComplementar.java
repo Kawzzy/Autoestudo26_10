7f64197a11ac6463767b9e190e8eafbfc82e19ed
Fernando Butzke <63071834+nandobutzke@users.noreply.github.com>
2020-11-03 01:26:38 -0300

    Update ProjetoDeLeiComplementar.java

--- src/ProjetoDeLeiComplementar.java
+++ src/ProjetoDeLeiComplementar.java
@@ -4,7 +4,17 @@ public class ProjetoDeLeiComplementar extends ProjetoDeLei {
 	
 	private String artigoLO;
 	private int qtdVotosFavoraveis;
-	
+	private Vereador vereador;
+
+
+	public Vereador getVereador() {
+		return vereador;
+	}
+
+	public void setVereador(Vereador vereador) {
+		this.vereador = vereador;
+	}
+
 	public ProjetoDeLeiComplementar(String titulo, LocalDate dtApres, LocalDate dtAprov, 
 			int numProjeto, String artigoLO, int qtdVF, 
 			String nomeVereador, Partido partidoVereador) {
@@ -33,11 +43,14 @@ public class ProjetoDeLeiComplementar extends ProjetoDeLei {
 	}
 
 	public void setQtdVotosFavoraveis(int qtdVotosFavoraveis) {
-		this.qtdVotosFavoraveis = qtdVotosFavoraveis;
+		this.qtdVotosFavoraveis += qtdVotosFavoraveis;
 	}
 
 	public String mostrar() {
-		String a = "";
-		return a;
+		String msg = "Título: " + this.getTitulo() + " \n" + "Data de Apresentação: " + super.getDataApresentacao() + 
+				" \n" + "Data de Aprovação: " + this.getDataAprovacao() + " \n" + "Número do Projeto: " + this.getNumeroProjeto() + "\n" + 
+				"Lei Orgânica: " + this.getArtigoLO() + "\n" + "Qtd votos Favoráveis: " + this.getQtdVotosFavoraveis();
+
+		return msg;
 	}
 }
